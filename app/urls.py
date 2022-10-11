06e59042c4352aa1ab1fcbc9fae51c0adb7e845d
Aniket Sharma <aniketsharma933577@gmail.com>
2022-10-11 23:29:26 +0530

    Procfile Added

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from app.views import home , login , signup , add_todo , signout , delete_todo, change_todo
 
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
    path('' , home , name='home' ), 
@@ -11,5 +13,8 @@ urlpatterns = [
    path('add-todo/' , add_todo ), 
    path('delete-todo/<int:id>' , delete_todo ), 
    path('change-status/<int:id>/<str:status>' , change_todo ), 
-   path('logout/' , signout ), 
+   path('logout/' , signout ),
+
+   url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+   url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
